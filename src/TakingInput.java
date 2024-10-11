c9ff015d713b3b6b5f60fbb7fd37e66a9c667af6
prajjwalRathore-39 <prajjwalrathore39@gmail.com>
2024-10-11 22:26:17 +0530

    Code to demonstrate Integer Input Using Scanner Class.

---
+++ src/TakingInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        System.out.println("The number inputted is: "+n);
+    }
+}
