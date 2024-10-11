c9d0af1a442b4b317737696dd510e3f8d92004a7
prajjwalRathore-39 <prajjwalrathore39@gmail.com>
2024-10-11 21:22:57 +0530

    Hello file updated

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello file created.");
+        System.out.println("jla gbk;e");
+        System.out.println("ieja gbk; egkbh");
+    }
 }
